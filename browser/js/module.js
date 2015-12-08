b4386651f837e0a91773cec644520bc566afa118
Sam Narisi <snarisi@gmail.com>
2015-12-08 16:37:24 -0500

    Add song list directive

--- browser/js/module.js
+++ browser/js/module.js
@@ -1,1 +1,62 @@
 var app = angular.module('juke', ['ui.router', 'ngMessages']);
+
+app.directive('sidebar', function () {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/sidebar.html'
+    }
+});
+
+app.directive('player', function (PlayerFactory) {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/player.html',
+        link: function (scope) {
+            
+            scope.getCurrentSong = PlayerFactory.getCurrentSong;
+            scope.isPlaying = PlayerFactory.isPlaying;
+            scope.forward = PlayerFactory.next;
+            scope.back = PlayerFactory.previous;
+
+            scope.getPercent = function () {
+                return (100 * PlayerFactory.getProgress()) + '%';
+            };
+
+            scope.toggle = function () {
+                if (PlayerFactory.isPlaying()) PlayerFactory.pause();
+                else PlayerFactory.resume();
+            };            
+        }
+    };
+});
+
+app.directive('albumList', function () {
+    return {
+        restrict: "E",
+        templateUrl: '/templates/album-list.html',
+        scope: {
+            albums: "="
+        } 
+    };
+});
+
+app.directive('songList', function (PlayerFactory) {
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/song-list.html',
+        scope: {
+            songs: '='
+        },
+        link: function (scope) {
+            scope.isCurrent = function (song) {
+                var current = PlayerFactory.getCurrentSong();
+                return current && current._id == song._id;
+            };
+            scope.start = function (song) {
+                PlayerFactory.start(song, scope.songs);
+            };
+
+        }
+
+    }
+})
